c0f65501d205c57640029b6caaae04532655a86f
Me-legna <111507103+Me-legna@users.noreply.github.com>
2023-01-27 00:00:53 -0500

    fix users table developer_alias column typo and migrate

--- app/models/user.py
+++ app/models/user.py
@@ -21,7 +21,7 @@ class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
-    developer_alias = db.Column(db.String(40, nullable=True, unique=True))
+    developer_alias = db.Column(db.String(40), nullable=True, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
