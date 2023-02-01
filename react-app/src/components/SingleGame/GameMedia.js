75ad3b8e47fd549b4d5222b039149d295c4cee12
Me-legna <111507103+Me-legna@users.noreply.github.com>
2023-02-01 00:13:11 -0500

    update gameMedia comp

--- react-app/src/components/SingleGame/GameMedia.js
+++ react-app/src/components/SingleGame/GameMedia.js
@@ -1,9 +1,61 @@
+import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
 
 
-function GameMedia(){
+function GameMedia() {
+    const singleGame = useSelector(state => state.games.singleGame)
+    const previewImage = singleGame.media?.find(img => img.is_preview === true)
+    const [selectedMedia, setSelectedMedia] = useState()
+
+    useEffect(() => {
+        setSelectedMedia(singleGame.media?.[0].url)
+    }, [singleGame])
 
     return (
-        <h1>GameMedia</h1>
+        <>
+            <h1>GameMedia</h1>
+            <figure>
+                <img src={selectedMedia} alt='selected-media'></img>
+            </figure>
+            <section>
+                <div>
+                    {singleGame.media?.map((media, idx) => (
+                        <img
+                            key={media.id}
+                            src={media.url}
+                            alt={`${singleGame.title} #${idx + 1}`}
+                            onClick={() => setSelectedMedia(media.url)}
+                        />
+                    ))}
+                </div>
+                <div>
+                    <img src={previewImage?.url} alt='game-preview'></img>
+                    <p>
+                        {singleGame.description}
+                    </p>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th>Reviews:</th>
+                                <td>Positive?</td>
+                            </tr>
+                            <tr>
+                                <th>Release Date:</th>
+                                <td>{singleGame.release_date}</td>
+                            </tr>
+                            <tr>
+                                <th>Developer:</th>
+                                <td>{singleGame.developer ? singleGame.developer : 'Anonymous Studios'}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+            <div>
+               <button>Wishlist?</button>
+            </div>
+        </>
+
     )
 }
 
--- react-app/src/components/StoreHomePage/AllGames.js
+++ react-app/src/components/StoreHomePage/AllGames.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { NavLink } from 'react-router-dom'
-import { load_all_games } from '../../store/games'
+import { NavLink, useHistory } from 'react-router-dom'
+import { load_all_games, load_one_game } from '../../store/games'
 import './HomePage.css'
 
 
@@ -9,54 +9,62 @@ function AllGames() {
     const allGamesObj = useSelector(state => state.games.allGames.byId)
     const allGamesArr = Object.values(allGamesObj)
     const dispatch = useDispatch()
+    const history = useHistory()
     console.log(allGamesArr)
 
     useEffect(() => {
         dispatch(load_all_games())
     }, [dispatch])
 
+    async function handleClick(gameId){
+        await dispatch(load_one_game(gameId))
+            .then(() => history.push(`/games/${gameId}`))
+
+    }
+
     return allGamesArr.length && (
         <>
             <div className='games-list-container'>
 
                 {allGamesArr.map(game => (
-                    <NavLink className='indiv-game-nav' key={game.title} to={`/games/${game.id}`}>
-                        <div className='indiv-game' >
-                            <div className='game-list-image-container'>
-                                <img className='game-list-image' src={game.media.find(image => image.is_preview === true).url} alt='game-img'></img>
+                    // <NavLink className='indiv-game-nav' key={game.title} to={`/games/${game.id}`}>
+                    <div key={game.id} className='indiv-game' onClick={() => handleClick(game.id)}>
+                        {console.log('game', game)}
+                        <div className='game-list-image-container'>
+                            <img className='game-list-image' src={game.media.find(image => image.is_preview === true).url} alt='game-img'></img>
+                        </div>
+                        <div>
+                            <div>
+                                {game.title}
                             </div>
                             <div>
-                                <div>
-                                    {game.title}
-                                </div>
-                                <div>
-                                    {game.systems.map(system => (
-                                        system === 'Windows'
+                                {game.systems.map(system => (
+                                    system === 'Windows'
+                                        ?
+                                        <i key={`${system.id}`} className="fa-brands fa-windows"></i>
+                                        : system === 'MacOS'
                                             ?
-                                            <i key={`${system.id}`} className="fa-brands fa-windows"></i>
-                                            : system === 'MacOS'
+                                            <i key={`${system.id}`} className="fa-brands fa-apple"></i>
+                                            : system === 'VaporOS + Linux'
                                                 ?
-                                                <i key={`${system.id}`} className="fa-brands fa-apple"></i>
-                                                : system === 'VaporOS + Linux'
-                                                    ?
-                                                    <i key={`${system.id}`} className="fa-solid fa-smog"></i>
-                                                    :
-                                                    <></>
-                                    ))}
-                                </div>
-                                <div>
-                                    {game.genres.map((genre, idx) => (
-                                        idx === 0
-                                            ? genre
-                                            : `, ${genre}`
-                                    ))}
-                                </div>
+                                                <i key={`${system.id}`} className="fa-solid fa-smog"></i>
+                                                :
+                                                <></>
+                                ))}
                             </div>
                             <div>
-                                {game.price > 0 ? `$${game.price}` : 'Free'}
+                                {game.genres.map((genre, idx) => (
+                                    idx === 0
+                                        ? genre
+                                        : `, ${genre}`
+                                ))}
                             </div>
                         </div>
-                    </NavLink>
+                        <div>
+                            {game.price > 0 ? `$${game.price}` : 'Free'}
+                        </div>
+                    </div>
+                    // </NavLink>
                 ))}
             </div>
         </>
