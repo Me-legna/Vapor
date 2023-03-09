c17127becd84bafb2c303b3dfae8b7cd597ae96d
Angel Gonzalez <111507103+Me-legna@users.noreply.github.com>
2023-03-09 17:18:43 -0600

    add pointer style and rerouting to ordersList to singleOrder

--- react-app/src/components/AllOrders/OrderListData.js
+++ react-app/src/components/AllOrders/OrderListData.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 function OrderListData() {
 	const allOrders = useSelector((state) => state.orders.allOrders.byId);
 	const ordersList = Object.values(allOrders);
+	const history = useHistory()
 	return (
 		<>
 			{ordersList.map((order) => {
 				// if (order.total === 0) return <React.Fragment key={order.id + order.type}></React.Fragment>;
 				return (
-					<tr key={order.id}>
+					<tr style={{cursor: 'pointer'}} onClick={() => history.push(`/orders/${order.id}`) } key={order.id}>
 						<td key={order.id}>{order.purchaseDate}</td>
 						<td>
 							{order.items.map((item) => (
