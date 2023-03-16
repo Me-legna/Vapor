e0d0f66655d10922e4674cf05b4f3d92778ea47c
Angel Gonzalez <111507103+Me-legna@users.noreply.github.com>
2023-03-16 16:43:07 -0500

    Update Library Component and CSS, Add container to AllOrders, Add padding to homepage gameslist images

--- react-app/src/components/AllOrders/index.js
+++ react-app/src/components/AllOrders/index.js
@@ -1,23 +1,25 @@
 import { useEffect } from "react";
+import Container from "react-bootstrap/Container";
 import { useDispatch, useSelector } from "react-redux";
 import { loadAllOrders } from "../../store/orders";
 import AllOrdersBody from "./body";
 import AllOrdersHeader from "./header";
 
 function AllOrders() {
-	const user = useSelector(state => state.session.user)
-	const dispatch = useDispatch()
+	const user = useSelector((state) => state.session.user);
+	const dispatch = useDispatch();
 
-	useEffect(()=>{
-		dispatch(loadAllOrders())
-	},[dispatch])
+	useEffect(() => {
+		dispatch(loadAllOrders());
+	}, [dispatch]);
 
 	return (
 		<div className="main">
-			<AllOrdersHeader />
-			<AllOrdersBody />
+			<Container>
+				<AllOrdersHeader />
+				<AllOrdersBody />
+			</Container>
 		</div>
-
 	);
 }
 
