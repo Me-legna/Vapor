2020a92e87a9229f7c465d7a85960694ad87620e
Angel Gonzalez <111507103+Me-legna@users.noreply.github.com>
2023-03-23 15:31:04 -0500

    update GameSummary component and styling

--- react-app/src/components/SingleGame/GameSummary.js
+++ react-app/src/components/SingleGame/GameSummary.js
@@ -26,18 +26,15 @@ function GameSummary() {
 				/>
 			</div>
 			<p className="description-ctn">
-				{/* {singleGame.description} */}
-				Become the king and fight over Europe in this fresh take on medieval
-				real-time grand strategy. Manage provinces, gather armies, initiate
-				diplomacy, and dabble in espionage. The path is open: become the true
-				Sovereign of your people and bring them to glory.
+				{singleGame.description}
+				
 			</p>
-			<div>
-				<Table className="game-detail-table">
+			<div className="game-table-ctn">
+				<Table borderless className="game-detail-table">
 					<tbody>
 						<tr>
-							<th>REVIEWS:</th>
-							<td>Positive?</td>
+							{/* <th>REVIEWS:</th>
+							<td>Positive?</td> */}
 						</tr>
 						<tr>
 							<th>RATING:</th>
@@ -47,6 +44,7 @@ function GameSummary() {
 							<th>RELEASE DATE:</th>
 							<td>{singleGame.release_date}</td>
 						</tr>
+						<br/>
 						<tr>
 							<th>DEVELOPER:</th>
 							<td>
@@ -60,63 +58,6 @@ function GameSummary() {
 			</div>
 			<div></div>
 		</div>
-		// <Container className="d-flex m-0 p-0">
-		// <div className="media-comp-container">
-		// 	<div className="media-left-container">
-		// 		<figure className="showcase-img-container">
-		// 			<img
-		// 				className="showcase-img"
-		// 				src={selectedMedia}
-		// 				onError={addDefaultSrc}
-		// 				alt="selected-media"
-		// 			></img>
-		// 		</figure>
-		// 		<div>
-		// 			{singleGame.media?.map((media, idx) => (
-		// 				<img
-		// 					className="small-media"
-		// 					key={media?.id}
-		// 					src={media?.url}
-		// 					onError={addDefaultSrc}
-		// 					alt={`${singleGame.title} #${idx + 1}`}
-		// 					onClick={() => setSelectedMedia(media?.url)}
-		// 				/>
-		// 			))}
-		// 		</div>
-		// 	</div>
-		// 	<div className="media-right-container">
-		// 		<img
-		// 			className="cover-img"
-		// 			src={singleGame.cover}
-		// 			onError={addDefaultSrc}
-		// 			alt="game-preview"
-		// 		></img>
-		// 		<p className="discription-p">{singleGame.description}</p>
-		// 		<table>
-		// 			<tbody>
-		// 				<tr>
-		// 					<th>RATING:</th>
-		// 					<td>{singleGame.rating}</td>
-		// 					{/* <th>REVIEWS:</th>
-		//                     <td>Positive?</td> */}
-		// 				</tr>
-		// 				<tr>
-		// 					<th>RELEASE DATE:</th>
-		// 					<td>{singleGame.release_date}</td>
-		// 				</tr>
-		// 				<tr>
-		// 					<th>DEVELOPER:</th>
-		// 					<td>
-		// 						{singleGame.developer
-		// 							? singleGame.developer
-		// 							: "Anonymous Studios"}
-		// 					</td>
-		// 				</tr>
-		// 			</tbody>
-		// 		</table>
-		// 	</div>
-		// </div>
-		// </Container>
 	);
 }
 
